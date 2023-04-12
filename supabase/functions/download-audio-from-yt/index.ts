dedd91a7360cc8184f5a1759817693808ba7a604
Mani Kandan <40218068+mkandan@users.noreply.github.com>
2023-04-12 13:33:34 -0700

    trashing

--- supabase/functions/download-audio-from-yt/index.ts
+++ supabase/functions/download-audio-from-yt/index.ts
@@ -2,24 +2,48 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import youtubedl from 'https://cdn.skypack.dev/pin/youtube-dl-exec@v2.4.0-G5Wz5BWSXvKAhwFKwDO1/mode=imports/optimized/youtube-dl-exec.js'
+import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 
-console.log("Hello from Functions!")
+import { corsHeaders } from '../_shared/cors.ts'
+
+console.log(`Function "browser-with-cors" up and running!`)
 
 serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
   }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
+  youtubedl('https://www.youtube.com/watch?v=6xKWiCMKKJg', {
+    dumpSingleJson: true,
+    noCheckCertificates: true,
+    noWarnings: true,
+    preferFreeFormats: true,
+    addHeader: ['referer:youtube.com', 'user-agent:googlebot'],
+  }).then((output: any) => console.log(output))
+
+  try {
+    const { name } = await req.json()
+    const data = {
+      message: `Hello download-audio-from-yt ${name}!`,
+    }
+
+    return new Response(JSON.stringify(data), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 200,
+    })
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
 })
 
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+/* To invoke:
+curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+  --header 'Content-Type: application/json' \
+  --data '{"name":"Functions"}'
+  */
