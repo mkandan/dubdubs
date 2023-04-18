ad7c2e039b88c1bca3c7146a9debd1e3c062472e
Mani Kandan <40218068+mkandan@users.noreply.github.com>
2023-04-17 18:52:02 -0700

    deploy attempt 1

---
+++ gcp-cloud-functions/main.py
@@ -0,0 +1,55 @@
+import os
+import time
+import functions_framework
+from pytube import YouTube
+import openai
+
+path_to_tmp_folder = 'tmp'  # local api on personal device
+
+
+@functions_framework.http
+def whisper_cap(request):
+    start_time = time.time()
+    # get youtube url and desired_language from request
+    request_json = request.get_json()
+
+    if request_json and 'yt_url' in request_json and 'desired_language' and 'api_key' in request_json:
+        yt_url = request_json['yt_url']
+        desired_language = request_json['desired_language']
+        api_key = request_json['api_key']
+
+        # download audio from YT
+        yt = YouTube(yt_url)
+        yt_title = yt.title
+        yt_description = yt.description
+        yt_stream = yt.streams.filter(only_audio=True).first()
+        yt_stream.download(
+            output_path=path_to_tmp_folder)
+        file_path = path_to_tmp_folder+'/'+yt_stream.default_filename
+
+        # run audio through Whisper
+        # openai.api_key = api_key
+        # audio_file = open(file_path, 'rb')
+        # transcript = openai.Audio.transcribe("whisper-1", audio_file,)
+
+        # delete file from local storage
+        os.remove(file_path)
+
+        return {"message": "success", "response_time": (time.time()-start_time), "yt_url": yt_url, "desired_language": desired_language, yt_title: yt_title, "yt_description": yt_description, }
+
+    # handle missing parameters
+    missing_params = []
+    required_params = ['yt_url', 'desired_language', 'api_key']
+    if request_json:
+        for param in required_params:
+            if param not in request_json:
+                missing_params.append(param)
+        if missing_params:
+            return {"message": "missing parameter", "required": missing_params, "response_time": (time.time()-start_time)}
+        # if all required parameters are present, but empty
+        else:
+            return {"message": "missing parameter", "required": required_params, "response_time": (time.time()-start_time)}
+
+    # handle empty request
+    else:
+        return {"message": "missing parameter", "required": ['yt_url', 'desired_language', 'api_key'], "response_time": (time.time()-start_time)}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,88 +1,9 @@
 import { type NextPage } from 'next'
 import Head from 'next/head'
 import { env } from '~/env.mjs'
-import axios, { AxiosResponse } from 'axios'
-import ffmpeg, { FfmpegCommand } from 'fluent-ffmpeg'
-import { Readable, Writable } from 'stream'
-import fs from 'fs'
+import axios from 'axios'
 
 const Home: NextPage = () => {
-  async function callWhisper(url: string, languageCode: string) {
-    try {
-      const response: AxiosResponse<ArrayBuffer> = await axios.get(url, {
-        responseType: 'arraybuffer',
-      })
-      // Making a stream out of the buffer
-      const inputStream = arrayBufferToStream(response.data)
-      // We want to avoid the 25 MB limitation and ensure that the audio file is within the acceptable size range for the API.
-      const resizedBuffer = await reduceBitrate(inputStream)
-      //  This step is necessary because the OpenAI API expects a stream as input for the audio file.
-      const resizedStream = bufferToReadableStream(resizedBuffer, 'audio.mp3')
-      console.log('resizedStream: ', resizedStream)
-      // const configuration = new Configuration({
-      //   apiKey: process.env.OPEN_API_KEY,
-      // })
-
-      // const openai = new OpenAIApi(configuration)
-      // let prompt = 'YOUR PROMPT'
-
-      // const resp = await openai.createTranscription(
-      //   resizedStream,
-      //   'whisper-1',
-      //   prompt,
-      //   'verbose_json',
-      //   0.8,
-      //   language_code,
-      //   { maxContentLength: Infinity, maxBodyLength: Infinity },
-      // )
-      // return resp.data
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  function arrayBufferToStream(buffer: ArrayBuffer) {
-    const readable = new Readable({
-      read() {
-        this.push(Buffer.from(buffer))
-        this.push(null)
-      },
-    })
-    return readable
-  }
-
-  function reduceBitrate(inputStream: Readable): Promise<Buffer> {
-    return new Promise<Buffer>((resolve, reject) => {
-      const outputChunks: Buffer[] = []
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-      ffmpeg(inputStream as any)
-        .audioBitrate(64) // low quality. You can update that
-        .on('error', reject)
-        .on('end', () => resolve(Buffer.concat(outputChunks)))
-        .format('mp3')
-        .pipe(
-          new Writable({
-            write(chunk: Buffer, encoding: string, callback: () => void) {
-              outputChunks.push(chunk)
-              callback()
-            },
-          }),
-        )
-    })
-  }
-
-  function bufferToReadableStream(buffer: Buffer, filename: string): Readable {
-    const readable: Readable = new Readable({
-      read() {
-        this.push(buffer)
-        this.push(null)
-      },
-    }) as never
-    readable.path = filename
-    readable._readableState.objectMode = false
-    return readable
-  }
-
   const handleEdgeFunction = () => {
     console.log('clicked brain at: ', new Date().toLocaleString())
     axios
@@ -123,12 +44,6 @@ const Home: NextPage = () => {
         <button
           className="rounded-lg bg-white p-6"
           // onClick={() => handleEdgeFunction()}
-          onClick={() =>
-            void callWhisper(
-              'https://www.youtube.com/watch?v=u7j--YMXZtA',
-              'en',
-            )
-          }
         >
           🧠 on {process.env.NODE_ENV}
         </button>
