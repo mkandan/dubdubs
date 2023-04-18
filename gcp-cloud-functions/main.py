1f383e1da5dbe32cca32bfc6894f6edf5ae68e65
Mani Kandan <40218068+mkandan@users.noreply.github.com>
2023-04-17 22:42:19 -0700

    feat:verbose transcript with timestamps wip: write to supabase, lvl 2 translation via gpt

--- gcp-cloud-functions/main.py
+++ gcp-cloud-functions/main.py
@@ -3,6 +3,7 @@ import time
 import functions_framework
 from pytube import YouTube
 import openai
+import json
 
 path_to_tmp_folder = 'tmp'  # local api on personal device
 
@@ -18,6 +19,13 @@ def whisper_cap(request):
         desired_language = request_json['desired_language']
         api_key = request_json['api_key']
 
+        # remove any playlist/mix (non single video) parameters from url
+        if '&' in yt_url:
+            yt_url = yt_url.split('&')[0]
+        # prevent shorts from being processed
+        if 'shorts' in yt_url:
+            return {"message": "shorts are not supported", "response_time": (time.time()-start_time)}
+
         # download audio from YT
         try:
             yt = YouTube(yt_url)
@@ -28,20 +36,30 @@ def whisper_cap(request):
             file_path = os.path.join(
                 path_to_tmp_folder, yt_stream.default_filename)
 
-        except Exception as e:
-            # handle the exception here
-            print(f"Error while working with audio via pytub: {e}")
-            return {'error': 'Error while working with audio via pytub', 'message': e}, 500
+        except PytubeError as e:
+            error_message = str(e)  # convert PytubeError object to string
+            response = {
+                "status": "error",
+                "message": error_message,
+            }
+            # return error response with status code 500
+            return json.dumps(response), 500
 
-        # run audio through Whisper
-        # openai.api_key = api_key
-        # audio_file = open(file_path, 'rb')
-        # transcript = openai.Audio.transcribe("whisper-1", audio_file,)
+        # run audio through Whisper -- $0.006 / minute (rounded to the nearest second)
+        openai.api_key = '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'
+        audio_file = open(file_path, 'rb')
+        transcript = openai.Audio.transcribe(
+            "whisper-1", audio_file, response_format="verbose_json")
+
+        # clean up transcript. removed avg_logprob, compression_ratio, no_speech_prob, seek, temperature, tokens, and transient
+        for segment in transcript['segments']:  # type: ignore
+            for key in ['avg_logprob', 'compression_ratio', 'no_speech_prob', 'seek', 'temperature', 'tokens', 'transient']:
+                segment.pop(key, None)
 
         # delete file from local storage
         os.remove(file_path)
 
-        return {"message": "success", "response_time": (time.time()-start_time), "yt_url": yt_url, "desired_language": desired_language, yt_title: yt_title, "yt_description": yt_description, }
+        return {"message": "success", "response_time": (time.time()-start_time), "yt_url": yt_url, "desired_language": desired_language, "yt_title": yt_title, "yt_description": yt_description, "transcript": transcript}
 
     # handle missing parameters
     missing_params = []
